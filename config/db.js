94c00f6e927f97abdb71a1b2b11c8f8c3e1d8409
Kaviya <kaviya@192.168.1.5>
2025-09-16 16:17:16 +0530

    updated db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI;
     if (!uri) throw new Error("MONGO_URI not set in env");
+    console.log("Trying to connect to MongoDB..."); 
+
     const conn = await mongoose.connect(uri);
     console.log("MongoDB Connected:", conn.connection.host);
   } catch (err) {
